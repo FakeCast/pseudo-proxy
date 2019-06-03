3cecf56e21a062ac1a422d75bb788a52773d27b9
Henrique Feitosa <henrique.feitosa@locaweb.com.br>
2019-06-03 18:50:23 -0300

    Added cors configuration to Sinatra

--- server.rb
+++ server.rb
@@ -1,12 +1,28 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'sinatra/cross_origin'
 require 'net/http'
 require 'openssl'
 
 set :bind, '0.0.0.0'
 
+configure do
+  enable :cross_origin
+end
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+options '*' do
+  response.headers['Allow'] = 'GET, PUT, POST, DELETE, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token'
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  200
+end
 
 get '/:protocol/*' do
   uri = URI("#{params[:protocol]}//#{params[:splat].first}")
